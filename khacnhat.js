7239e4276bf609d2cf15ff6e1c2fe0715a6f43ae
Your Name <you@example.com>
2023-07-05 22:01:38 +0700

    first commit

--- khacnhat.js
+++ khacnhat.js
@@ -1,4 +1,52 @@
-function self ()
-let x = document.forms[form][N].value;
-if (x== "17")
-    
+var knhat = "NKN"
+let nhat = "nkn"
+const number = 6 ;
+console.log(knhat) 
+    var  numbera = 5;
+    var numberb = -2;
+    var c = a+b  // a= 5    b=-2   c=-3//
+    console.log(c)
+    var d = a-b
+    var e = a/b
+    var  a = 5
+    var  b = 5 
+    if (a===b){
+        console.log( " so a va b bang nhau")   
+    }else {
+        console.log( " so a va b khong bang nhau")
+    } 
+
+    var a = 19 
+    if(a>18 ){
+        console.log("dieu kien dung ")
+    }else{
+        console.log("khong dung")
+    }
+
+
+    var mark = 65 
+    if(mark < 50){
+        console.log("truot")
+        
+    }
+    if(mark > 50 && mark < 60){
+        console.log(" trung binh ")
+    }
+    if(mark >=60 && mark <70 ){
+        console.log(" kha")
+    }
+    if(mark >= 70 && mark < 80){
+        console.log("gioi")
+    }
+    else{
+        console.log("xuat sac")
+    }
+ var student= [ "Nam","The Anh","Hiep"]
+ var length = student.length
+ console.log(length)
+ var push =student.push [ "Nhat","Hoa"]
+ console.log(push)
+ var pop= student.pop 
+ console.log(pop)
+ student.length
+ student.index 
